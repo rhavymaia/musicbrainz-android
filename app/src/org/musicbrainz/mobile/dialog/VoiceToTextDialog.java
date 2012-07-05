a5023e470e7a348de27da36b4996bd71bbf69c5b
rhavymaia <rmg3@cin.ufpe.br>
2012-07-05 15:13:45 -0300

    Adding voice converter to text on search dashboard

---
+++ app/src/org/musicbrainz/mobile/dialog/VoiceToTextDialog.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2012 Jamie McDonald
+ * 
+ * This file is part of MusicBrainz for Android.
+ * 
+ * MusicBrainz for Android is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * MusicBrainz for Android is distributed in the hope that it 
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MusicBrainz for Android. If not, see 
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package org.musicbrainz.mobile.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class VoiceToTextDialog extends Dialog implements OnItemClickListener {
+
+	public VoiceToTextDialog(Context context) {
+		super(context);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+
+	}
+}
